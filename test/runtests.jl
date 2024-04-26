5cbbd23c1d57d550896e29cbdcb9cdd4fa7d8785
Edward <edward.wall@unite.eu>
2024-04-26 11:41:10 +0200

    hopefully fixixing moto server installation in CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using TestItemRunner
 const conda_package_path = "$(dirname(@__DIR__))/.testpython"
 ENV["CONDA_JL_HOME"] = conda_package_path
 if !isfile("$(conda_package_path)/bin/moto_server")
+    mkpath(conda_package_path)
     Pkg.build("Conda")
     Conda.add("moto[server]", conda_package_path; channel="conda-forge")
 end
